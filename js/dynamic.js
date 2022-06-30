65b6f4530db9bae6de4c45ca46696779ae73b6aa
Ben Trainor <benjammintrain@gmail.com>
2022-06-30 11:12:34 -0500

    Coming right along smoothly

--- js/dynamic.js
+++ js/dynamic.js
@@ -2,6 +2,15 @@
 // Dynamically setting background images
 // Get inner HTML content for image path and set background image URL
 
-var dynamicBGImage;
-dynamicBGImage = document.getElementsByClassName('foo');
-console.log(dynamicBGImage.length);
+document.addEventListener("DOMContentLoaded", function(e) {
+
+    var dynamicBGImage = document.getElementsByClassName('img-container-dynamic');
+    var sourceFile;
+    
+    for (let i = 0; i < dynamicBGImage.length; i++) {
+        sourceFile = dynamicBGImage.item(i).firstElementChild.innerHTML;
+        console.log("Setting background image to " + sourceFile);
+        dynamicBGImage.item(i).style.backgroundImage = "url(" + sourceFile + ")";
+    }
+
+})
