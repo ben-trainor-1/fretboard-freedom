2f400f15ec330d7dba1dbf0f4f36f926d60aa503
Ben Trainor <benjammintrain@gmail.com>
2022-06-30 10:04:08 -0500

    Layout coming along. Script not working yet

---
+++ js/dynamic.js
@@ -0,0 +1,7 @@
+
+// Dynamically setting background images
+// Get inner HTML content for image path and set background image URL
+
+var dynamicBGImage;
+dynamicBGImage = document.getElementsByClassName('foo');
+console.log(dynamicBGImage.length);
